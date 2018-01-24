9ebaeef2725e788e1978925418d2982f19b491ba
Sharon Hains <hainssharon@gmail.com>
2018-01-24 00:40:26 -0700

    first commit

---
+++ app/src/main/java/com/example/sharonhains/subbook/Subscription.java
@@ -0,0 +1,17 @@
+package com.example.sharonhains.subbook;
+
+import java.util.Date;
+
+/**
+ * Created by Sharon Hains on 2018-01-23.
+ */
+
+public class Subscription {
+
+    private String name;
+    private Date date;
+    private String comment;
+    private int charge;
+
+    public Subscription ()
+}
